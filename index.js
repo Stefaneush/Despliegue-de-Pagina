7a4f437c34db8466573d446dff6a50d6436ebb55
Stefaneush <stefanoedferro60@gmail.com>
2024-11-06 12:10:58 -0300

    postgres añadido

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, { query } from 'express';
+import {config} from "dotenv";
+import pg from "pg";
+
+config()
 
 const app = express();
 
+const pool = new pg.Pool({
+    
+    connectionString: process.env.DATABASE_URL
+
+})
+
 app.get('/', (req, res) => {
     res.send('¡Hola Mundo!, Soy Programador');
+});
+
+app.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
 });
 
 app.listen(3000)
