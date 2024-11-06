b3e5028d4cd042dd1e3fa1c777158379fcdd5b4b
Stefaneush <stefanoedferro60@gmail.com>
2024-11-06 16:26:25 -0300

    Console log

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 
 app.get('/ping', async (req, res) => {
     const result = await pool.query('SELECT * FROM usuarios')
-    return res.json(result.rows)
+    console.log(result.rows)
 });
 
 app.listen(3000)
