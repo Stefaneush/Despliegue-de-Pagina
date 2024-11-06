8331bb02475252ce0cd73cac45ec4927c33134a6
Stefaneush <stefanoedferro60@gmail.com>
2024-11-06 12:33:38 -0300

    Mostrar usuarios

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/ping', async (req, res) => {
-    const result = await pool.query('SELECT NOW()')
-    return res.json(result.rows[0])
+    const result = await pool.query('SELECT * FROM usuarios')
+    return res.json(result.rows)
 });
 
 app.listen(3000)
