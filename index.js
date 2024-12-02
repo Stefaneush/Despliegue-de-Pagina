8b14ce4aa818ca5799c10362fe178d600eeae40f
Stefaneush <stefanoedferro60@gmail.com>
2024-12-02 00:24:33 -0300

    despliegue 1

--- index.js
+++ index.js
@@ -1,69 +1,24 @@
 import express from 'express';
 import { config } from "dotenv";
 import pg from "pg";
-import bodyParser from 'body-parser';
-import cors from 'cors';
-const path = require('path');
 
 // Configuración inicial
 config();
+
+
 const app = express();
-
-// Middlewares
-app.use(bodyParser.json());
-app.use(cors());
-
-// Configuración de la base de datos
-const pool = new pg.Pool({
+const pool =  new pg.Pool({
     connectionString: process.env.DATABASE_URL
-});
+})
 
-
-// Rutas CRUD para usuarios
-
-// Servir archivos estáticos (HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Ruta para mostrar la página HTML
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.send('¡Hola Mundo!');
 });
 
-// Ruta para insertar un nuevo usuario en la base de datos
-app.post('/addUser', async (req, res) => {
-    const { nombre, correo, telefono } = req.body;
-
-    // Validar los datos
-    if (!nombre || !correo || !telefono) {
-        return res.status(400).json({ error: 'Todos los campos son obligatorios' });
-    }
-
-    try {
-        // Insertar los datos del usuario en la base de datos
-        const result = await pool.query(
-            'INSERT INTO usuarios (nombre, correo, telefono) VALUES ($1, $2, $3) RETURNING *',
-            [nombre, correo, telefono]
-        );
-        
-        // Enviar los datos del usuario recién creado como respuesta
-        res.status(201).json(result.rows[0]);
-    } catch (error) {
-        console.error('Error al insertar el usuario:', error);
-        res.status(500).json({ error: 'Error al insertar el usuario' });
-    }
+app.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT * from usuarios')
+    return res.json(result.rows[0])
 });
-
-// Ruta para obtener todos los usuarios de la base de datos
-app.get('/usuarios', async (req, res) => {
-    try {
-        const result = await pool.query('SELECT * FROM usuarios');
-        res.json(result.rows);
-    } catch (error) {
-        console.error('Error al obtener los usuarios:', error);
-        res.status(500).json({ error: 'Error al obtener los usuarios' });
-    }
-});
-
 
 app.listen(3000)
 console.log("server on port ", 3000)
