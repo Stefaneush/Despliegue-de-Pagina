3fbd9cf3ac3c633b5d805771a14efac9ce4fc372
Stefaneush <stefanoedferro60@gmail.com>
2024-12-02 01:07:05 -0300

    despliegue 7

--- index.js
+++ index.js
@@ -13,12 +13,22 @@ const pool = new pg.Pool({
 })
 
 app.get('/', async (req, res) => {
-    const result = await pool.query("INSERT INTO usuarios (nombre, correo, telefono) VALUES ('Emir Pérez', 'juan@example.com', '123456789'); ")
+    res.send("principal")
 });
 
-app.get('/ping', async (req, res) => {
+app.get('/create', async (req, res) => {
+    const result = await pool.query("INSERT INTO usuarios (nombre, correo, telefono) VALUES ('Emir Pérez', 'juan@example.com', '123456789'); ")
+    res.send("se creo el usuario")
+});
+
+app.get('/select', async (req, res) => {
     const result = await pool.query('SELECT * FROM usuarios')
     return res.json(result.rows)
+});
+
+app.get('/update', async (req, res) => {
+    const result = await pool.query("UPDATE usuarios SET nombre = 'Maurisio Hermoso', correo = 'maurisiaxd@example.com' WHERE id = 1;")
+    res.send("se actualizo el usuario")
 });
 
 
