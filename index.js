edea0e2f779b254920334f54c2565839c64a3dd9
Stefaneush <stefanoedferro60@gmail.com>
2024-12-02 01:52:19 -0300

    despliegue 12

--- index.js
+++ index.js
@@ -12,26 +12,29 @@ const pool = new pg.Pool({
 
 })
 
-app.get('/', async (req, res) => {
+app.post('/', async (req, res) => {
     res.send("principal")
 });
 
-app.get('/create', async (req, res) => {
-    const result = await pool.query("INSERT INTO usuarios (nombre, correo, telefono) VALUES ('Emir Pérez', 'juan@example.com', '123456789'); ")
+app.post('/create', async (req, res) => {
+
+    const { nombre, correo, telefono } = req.body;
+
+    const result = await pool.query("INSERT INTO usuarios (nombre, correo, telefono) VALUES ('$1', '$2', '$3'); " , [nombre, correo, telefono])
     res.send("se creo el usuario")
 });
 
-app.get('/select', async (req, res) => {
+app.post('/select', async (req, res) => {
     const result = await pool.query('SELECT * FROM usuarios')
     return res.json(result.rows)
 });
 
-app.get('/update', async (req, res) => {
+app.post('/update', async (req, res) => {
     const result = await pool.query("UPDATE usuarios SET nombre = 'Maurisio Hermoso', correo = 'maurisiaxd@example.com' WHERE id = 1;")
     res.send("se actualizo el usuario")
 });
 
-app.get('/delete', async (req, res) => {
+app.post('/delete', async (req, res) => {
     const result = await pool.query("DELETE FROM usuarios WHERE id = 3;")
     res.send("se elimino el usuario")
 });
