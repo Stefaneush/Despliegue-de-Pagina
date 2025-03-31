b60009ec6d4cb32d63a825a20d258245d9743d4b
unknown <Stefanoedferro60@gmail.com>
2025-03-31 16:50:08 -0300

    Despliegue 21

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/create', async (req, res) => {
     const { nombre, correo, telefono } = req.body;
 
     const result = await pool.query("INSERT INTO usuarios (nombre, correo, telefono) VALUES ($1, $2, $3); " , [nombre, correo, telefono])
-    res.send("El usuario ha sido creado exitosamente")
+    res.send("El usuario ha sido creado exitosamente.")
 });
 
 app.get('/select', async (req, res) => {
